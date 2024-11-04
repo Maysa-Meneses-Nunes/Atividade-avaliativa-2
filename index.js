129708e3cbc303a3a8d43fa8adaa96cd2e4266b6
Maysa Meneses Nunes <maysanunes764@gmail.com>
2024-11-04 00:39:36 -0200

    finalizado

--- index.js
+++ index.js
@@ -5,17 +5,18 @@ const app = express();
 const porta = 3000;
 const host = '0.0.0.0';
 
-function cadastro(req, resp){
+var listaClientes = [];
+function cadastroClienteView(req, resp){
  resp.send(`
      <html> 
      <head>
-                <title>Cadastro de Alunos</title>
+                <title> Cadastro </title>
                 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     </head>
     <body>  
     <div class="container text-center">
-                        <h1 class="mb-5">Cadastro de Alunos</h1>
-                        <form method="POST" action="/cadastrarAluno" class="border p-3 row g-3" novalidate>
+                        <h1 class="mb-5">Cadastro</h1>
+                        <form method="POST" action="/cadastrarCliente" class="border p-3 row g-3" novalidate>
                             <div class="col-md-4">
                                 <label for="nome" class="form-label">Nome</label>
                                 <input type="text" class="form-control" id="nome" name="nome"  placeholder="Digite seu nome" required>
@@ -26,15 +27,15 @@ function cadastro(req, resp){
     
                              </div>
                              <div class="col-md-4">
-                                <label for="email" class="form-label">email</label>
+                                <label for="CPF" class="form-label">CPF</label>
                                 <div class="input-group has-validation">
-                                    <span class="input-group-text" id="inputGroupPrepend">@</span>
-                                    <input type="text" class="form-control" id="email" name="email" required>
+                                    <span class="input-group-text" id="inputGroupPrepend"></span>
+                                    <input type="text" class="form-control" id="CPF" name="CPF" required>
                                 </div>
                             </div>
                             <div class="col-md-6">
-                                <label for="cidade" class="form-label">Cidade</label>
-                                  <input type="text" class="form-control" id="cidade" name="cidade" required>
+                                <label for="endereço" class="form-label">endereço</label>
+                                  <input type="text" class="form-control" id="endereço" name="endereço" required>
                             </div>
                             <div class="col-md-3">
                                 <label for="estado" class="form-label">UF</label>
@@ -69,8 +70,8 @@ function cadastro(req, resp){
                                 </select>
                             </div>
                             <div class="col-md-3">
-                                <label for="cep" class="form-label">Cep:</label>
-                                <input type="text" class="form-control" id="cep" name="cep" required>
+                                <label for="numero" class="form-label">numero</label>
+                                <input type="text" class="form-control" id="cep" name="numero" required>
                             </div>
                             <div class="col-12">
                                 <button class="btn btn-primary" type="submit">Cadastrar</button>
@@ -82,50 +83,27 @@ function cadastro(req, resp){
      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     </html>
     `)
-    function menuView(req, resp){
-        resp.send(`
-            <html>
-                <head>
-                    <title>Cadastro de Alunos</title>
-                    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-                </head>
-                <body>
-                    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-                        <div class="container-fluid">
-                            <a class="navbar-brand" href="#">MENU</a>
-                            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                                <div class="navbar-nav">
-                                    <a class="nav-link active" aria-current="page" href="/cadastrarAluno">Cadastrar Aluno</a>
-                                </div>
-                            </div>
-                        </div>
-                    </nav>
-                </body>
-                <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-            </html>
-            `);
-    }
-    
-    function cadastrarAluno(req, resp){
+
+    function cadastrarCliente(req, resp){
         //recuperar os dados do formulário enviados para o servidor
         const nome      = req.body.nome;
         const sobrenome = req.body.sobrenome;
-        const email     = req.body.email;
-        const cidade    = req.body.cidade;
+        const  CPF     = req.body.CPF;
+        const endereço    = req.body.endereço;
         const estado    = req.body.estado;
-        const cep       = req.body.cep;
+        const numero      = req.body.numero;
     
-        const aluno = {nome, sobrenome, email, cidade, estado, cep};
+        const cliente = {nome, sobrenome, CPF, endereço, estado, numero};
     
         //adicionar o aluno na lista
-        listaAlunos.push(aluno);
+        listaClientes.push(cliente);
     
         //mostrar a lista de alunos já cadastrados
         
         resp.write(`
             <html>
                 <head>
-                    <title>Lista de alunos</title>
+                    <title>Clientes Cadastrados</title>
                     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
                     <meta charset="utf-8">
                 </head>
@@ -135,31 +113,29 @@ function cadastro(req, resp){
                         <tr>
                             <th scope="col">Nome</th>
                             <th scope="col">Sobrenome</th>
-                            <th scope="col">Email</th>
-                            <th scope="col">Cidade</th>
+                            <th scope="col">CPF</th>
+                            <th scope="col">endereço</th>
                             <th scope="col">Estado</th>
-                            <th scope="col">Cep</th>
+                            <th scope="col">numero</th>
                         </tr>
                     </thead>
                     <tbody>`);
-                    //adicionar as linhas da tabela
-                    //para cada aluno, nós devemos criar uma linha na tabela
-                    for (var i = 0; i < listaAlunos.length; i++){
+
+                    for (var i = 0; i < listaClientes.length; i++){
                         resp.write(`<tr>
-                                        <td>${listaAlunos[i].nome}</td>
-                                        <td>${listaAlunos[i].sobrenome}</td>
-                                        <td>${listaAlunos[i].email}</td>
-                                        <td>${listaAlunos[i].cidade}</td>
-                                        <td>${listaAlunos[i].estado}</td>
-                                        <td>${listaAlunos[i].cep}</td>
+                                        <td>${listaClientes[i].nome}</td>
+                                        <td>${listaClientes[i].sobrenome}</td>
+                                        <td>${listaClientes[i].CPF}</td>
+                                        <td>${listaClientes[i].endereço}</td>
+                                        <td>${listaClientes[i].estado}</td>
+                                        <td>${listaClientes[i].numero}</td>
                                     </tr>
                             `);
                     }
     
         resp.write(`</tbody> 
                 </table>
-                <a class="btn btn-primary" href="/cadastrarAluno">Continuar Cadastrando</a>
-                <a class="btn btn-secondary" href="/">Voltar para o Menu</a>
+                <a class="btn btn-primary" href="/cadastrarCliente">Continuar Cadastrando</a>
                 </body>
                 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
             </html>
@@ -168,10 +144,9 @@ function cadastro(req, resp){
         resp.end();//será enviada a resposta
     }
     
-    app.get('/', menuView);
-    app.get('/cadastrarAluno', cadastroAlunoView); //enviar o formulário para cadastrar alunos
+    app.get('/cadastrarCliente', cadastroClienteView); //enviar o formulário para cadastrar alunos
     //a novidade desta aula é o método POST
-    app.post('/cadastrarAluno',cadastrarAluno);
+    app.post('/cadastrarCliente',cadastrarCliente);
     
 }
 
